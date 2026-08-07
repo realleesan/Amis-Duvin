c5517e6897cc41f66ba52dc035791a141cb662e4
realleesan <baominhkpkp@gmail.com>
2026-08-07 14:39:06 +0700

    feat: add static asset routing support for PHP built-in web server in index.php

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
 
+// Handle static assets for PHP built-in web server
+if (php_sapi_name() === 'cli-server') {
+    $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $file = __DIR__ . $url;
+    if ($url !== '/' && is_file($file)) {
+        return false;
+    }
+}
+
 // Single Entry Point
 require_once __DIR__ . '/core/App.php';
 
 $app = new \Core\App();
 $app->run();
+
